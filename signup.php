be2c6ac88039ba131cab1e1416ccdd880a31b9f4
Murali Mohan M <muralisocial123@gmail.com>
2022-09-07 01:57:06 +0000

    Created signup file

---
+++ signup.php
@@ -0,0 +1,15 @@
+<?php
+    
+$showAlert = false; 
+$showError = false; 
+$exists=false;
+    
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+      
+    // Include file which makes the
+    // Database Connection.
+    include 'dbconnect.php';   
+    
+    $username = $_POST["username"]; 
+    $password = $_POST["password"]; 
+    $cpassword = $_POST["cpassword"];
